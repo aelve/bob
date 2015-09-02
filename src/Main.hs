5980a9b92c0541577387fbd9f01925fc4d482c1e
Artyom <yom@artyom.me>
2015-09-03 00:11:31 +0300

    Simple GUI

--- src/Main.hs
+++ src/Main.hs
@@ -11,12 +11,14 @@ module Main where
 import Data.Maybe
 import Data.Foldable
 -- Lenses
-import Lens.Micro
+import Lens.Micro hiding (set)
 -- Text
 import Text.Printf
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import Data.Text (Text)
+-- GUI
+import Graphics.UI.Gtk
 -- Files
 import System.FilePath
 -- Data files
@@ -45,11 +47,60 @@ matchRule query Rule{..} = do
 matchRules :: Text -> [Rule] -> [(Text, Text)]
 matchRules query = mapMaybe (matchRule query)
 
+runGUI :: [Rule] -> IO ()
+runGUI rules = do
+  initGUI
+
+  window <- windowNew
+  window `on` objectDestroy $ mainQuit
+
+  searchEntry <- entryNew
+
+  -- create a new list model
+  model <- listStoreNew []
+  view <- treeViewNewWithModel model
+
+  treeViewSetHeadersVisible view True
+
+  -- add a couple columns
+  colChar <- treeViewColumnNew
+  colRule <- treeViewColumnNew
+
+  treeViewColumnSetTitle colChar ("Character" :: Text)
+  treeViewColumnSetTitle colRule ("Rule"      :: Text)
+
+  rendererChar <- cellRendererTextNew
+  rendererRule <- cellRendererTextNew
+
+  cellLayoutPackStart colChar rendererChar True
+  cellLayoutPackStart colRule rendererRule True
+
+  cellLayoutSetAttributes colChar rendererChar model $ \row ->
+    [ cellText := snd row ]
+  cellLayoutSetAttributes colRule rendererRule model $ \row ->
+    [ cellText := fst row ]
+  layout <- tableNew 2 1 False  -- 2 rows, 1 column, autostretching = off
+  tableAttachDefaults layout searchEntry 0 1 0 1
+  tableAttachDefaults layout view 0 1 1 2
+  window `containerAdd` layout
+
+  set layout [
+    tableChildYOptions searchEntry := [Fill] ]
+
+  treeViewAppendColumn view colChar
+  treeViewAppendColumn view colRule
+
+  searchEntry `on` editableChanged $ do
+    query <- get searchEntry entryText
+    listStoreClear model
+    for_ (matchRules query rules) $ \(name, result) ->
+      listStoreAppend model (name, result)
+
+  widgetShowAll window
+  mainGUI
+
 main :: IO ()
 main = do
   dataDir <- getDataDir
   rules <- readRules <$> T.readFile (dataDir </> "rules/main.rules")
-  query <- T.getLine
-  putStrLn ""
-  for_ (matchRules query rules) $ \(name, result) ->
-    printf "%s    (%s)\n" (T.unpack result) (T.unpack name)
+  runGUI rules
