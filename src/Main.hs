c36fd3059645fe3d6251e753b5c4381e13d4a7cd
Artyom <yom@artyom.me>
2015-09-07 03:41:47 +0300

    References and better warnings

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE
 RecordWildCards,
 OverloadedStrings,
-ScopedTypeVariables
+ScopedTypeVariables,
+FlexibleContexts
   #-}
 
 
@@ -13,12 +14,15 @@ import Data.Maybe
 import Data.Foldable
 import Data.Traversable
 import Data.Monoid
+import Data.Tuple
 import Control.Applicative
 import Control.Monad
+-- Monads
+import Control.Monad.Writer
 -- Lenses
 import Lens.Micro.GHC hiding (set)
 -- Lists
-import Data.List (permutations)
+import Data.List (permutations, union)
 -- Text
 import Text.Printf
 import qualified Data.Text as T
@@ -27,15 +31,12 @@ import Data.Text (Text)
 import Data.Char
 -- Parsing
 import Text.Parsec hiding ((<|>), many, optional)
-import Text.Parsec.Text
 -- Containers
 import qualified Data.Map as M
 import Data.Map (Map)
 import GHC.Exts (fromList)
 -- GUI
 import Graphics.UI.Gtk
--- liftIO
-import Control.Monad.IO.Class (liftIO)
 -- Clipboard
 import System.Hclip
 -- Files
@@ -55,55 +56,71 @@ type RuleName = Text
 
 type Mapping = Map Pattern Entity
 
+-- | Inverted 'Mapping' (used for references).
+type Entities = Map Entity [Pattern]
+
 data Generator
   = Literal Pattern
   | AnyOf [Generator]
   | Permutation [Generator]
+  | Reference Entity
   deriving (Show)
 
-literalP :: Parser Pattern
+literalP :: WarnParser Pattern
 literalP = T.pack <$> asum [
   some literalChar,
   between (char '\'') (char '\'') (many quotedChar) ]
   where
     literalChar = satisfy $ \x ->
       or [isSymbol x, isPunctuation x, isAlphaNum x] &&
-      x `notElem` ("\"'()[]{}" :: String)
+      x `notElem` ("\"'`()[]{}" :: String)
     quotedChar = asum [
       try (string "''") >> pure '\'',
       satisfy $ \x -> not $ or [isSpace x, x == '\''] ]
 
-generatorP :: Parser Generator
+generatorP :: WarnParser Generator
 generatorP = many (char ' ') *> asum [
   Literal <$> literalP,
   AnyOf <$> between (char '(') (char ')') (some generatorP),
-  Permutation <$> between (char '{') (char '}') (some generatorP) ]
+  Permutation <$> between (char '{') (char '}') (some generatorP),
+  Reference <$> between (char '`') (char '`') literalP ]
 
 data Matcher
   = Zip Text Text (Maybe Generator)
   | ManyToOne Generator Entity
   deriving (Show)
 
-evalGenerator :: Generator -> [Pattern]
-evalGenerator (Literal x) = [x]
-evalGenerator (AnyOf gs) = concatMap evalGenerator gs
-evalGenerator (Permutation gs) = do
-  perm :: [[Pattern]] <- permutations (map evalGenerator gs)
-  chosen :: [Pattern] <- sequence perm
-  return (mconcat chosen)
+evalGenerator :: Entities -> Generator -> Warn [Pattern]
+evalGenerator _  (Literal x) = return [x]
+evalGenerator es (AnyOf gs) = concat <$> mapM (evalGenerator es) gs
+evalGenerator es (Permutation gs) = do
+  ps :: [[Pattern]] <- mapM (evalGenerator es) gs
+  return $ do
+    perm :: [[Pattern]] <- permutations ps
+    chosen :: [Pattern] <- sequence perm
+    return (mconcat chosen)
+evalGenerator es (Reference x) = case M.lookup x es of
+  Nothing -> do
+    warn (printf "‘%s’ was referenced but wasn't defined yet" (T.unpack x))
+    return []
+  Just pats -> return pats
 
-evalMatcher :: Matcher -> Mapping
-evalMatcher (Zip lineA lineB mbGen) = do
+evalMatcher :: Entities -> Matcher -> Warn Mapping
+evalMatcher es (Zip lineA lineB mbGen) = do
   let as = T.chunksOf 1 lineA
       bs = T.chunksOf 1 lineB
-  let additions = maybe [""] evalGenerator mbGen
-  fromList $ concat $ do
+  additions <- case mbGen of
+    Nothing  -> return [""]
+    Just gen -> evalGenerator es gen
+  return $ fromList $ concat $ do
     (a, b) <- zip as bs
     addition <- additions
-    return [(addition <> a, b), (a <> addition, b)]
-evalMatcher (ManyToOne g y) = fromList (zip (evalGenerator g) (repeat y))
+    return [(addition <> a, b), (a <> addition, b), (b, b)]
+evalMatcher es (ManyToOne g y) = do
+  pats <- evalGenerator es g
+  return $ fromList $ zip (y:pats) (repeat y)
 
-matcherP :: Parser Matcher
+matcherP :: WarnParser Matcher
 matcherP = asum [zipP, manyToOneP]
   where
     -- Literals/generators/etc can go on a new line, but then they have to be
@@ -129,28 +146,37 @@ data Rule = Rule {
   ruleMapping :: Mapping }
   deriving (Show)
 
-ruleP :: Parser Rule
+ruleP :: WarnParser Rule
 ruleP = do
   name <- currentLine
-  mappings <- (evalMatcher <$> matcherP) `endBy1` newline
-  -- Find matches assigned to more than one thing (e.g. “<<” meaning both ‘↞’
-  -- and ‘↢’) – it's allowed in different rules, but not in the same rule.
-  let combinedMapping = M.unionsWith (++) $
-        over (each.each) (:[]) mappings
-  let mappingErrors = do
-        (k, vs) <- M.toList combinedMapping
-        guard (length vs /= 1)
-        return $ printf
-          "“%s” in rule “%s” corresponds to more than 1 thing: %s"
-          (T.unpack k) (T.unpack name)
-          (T.unpack (T.intercalate ", " vs))
-  unless (null mappingErrors) $
-    fail (unlines mappingErrors)
-  return $ Rule {
-    ruleName    = name,
-    ruleMapping = mconcat mappings }
+  let warningHeader = printf "warnings in rule ‘%s’:" (T.unpack name) :: String
+      nameWarnings = warnCensor $ \ws ->
+        if null ws then [] else warningHeader : map ("  " ++) ws
+  nameWarnings $ do
+    matchers <- matcherP `endBy1` newline
+    -- Evaluate all matchers, combining generated mappings as we go along and
+    -- passing them to each evaluator (so that references could be resolved).
+    let go _ [] = return []
+        go patterns (matcher:rest) = do
+          mapping <- evalMatcher patterns matcher
+          let patterns' = M.unionWith union (invertMap mapping) patterns
+          (mapping:) <$> go patterns' rest
+    mappings <- warnLift $ go mempty matchers
+    -- Find matches assigned to more than one thing (e.g. “<<” meaning both
+    -- ‘↞’ and ‘↢’) – it's allowed in different rules, but not in the same
+    -- rule.
+    let combinedMapping = M.unionsWith union $
+          over (each.each) (:[]) mappings
+    for_ (M.toList combinedMapping) $ \(k, vs) ->
+      when (length vs /= 1) $
+        warnLift $ warn $
+          printf "“%s” corresponds to more than 1 thing: %s"
+                 (T.unpack k) (T.unpack (T.intercalate ", " vs))
+    return $ Rule {
+      ruleName    = name,
+      ruleMapping = mconcat mappings }
 
-rulesP :: Parser [Rule]
+rulesP :: WarnParser [Rule]
 rulesP = ruleP `sepBy1` some newline
 
 matchRule :: Pattern -> Rule -> Maybe (RuleName, Entity)
@@ -260,16 +286,49 @@ main = do
                getDirectoryContents (dataDir </> "rules")
   rules <- fmap concat . for ruleFiles $ \ruleFile -> do
     let path = dataDir </> "rules" </> ruleFile
-    res <- parse rulesP ruleFile <$> T.readFile path
+    res <- warnParse rulesP ruleFile <$> T.readFile path
     case res of
-      Left err -> putStrLn (show err) >> return []
-      Right rules -> return rules
+      Left err -> do
+        putStrLn (show err)
+        return []
+      Right (rules, warnings) -> do
+        mapM_ putStrLn warnings
+        return rules
   runGUI rules
 
-currentLine :: Parser Text
+currentLine :: WarnParser Text
 currentLine = asum [
   endOfLine >> pure "",
   do x  <- anyChar
      xs <- anyChar `manyTill` try (eof <|> void endOfLine)
      pure (T.pack (x:xs)) ]
 
+type Warn a = Writer [String] a
+
+warn :: String -> Warn ()
+warn s = tell [s]
+
+type WarnParser a = Parsec Text [String] a
+
+warnLift :: Warn a -> WarnParser a
+warnLift x = do
+  let (a, w) = runWriter x
+  updateState (++ w)
+  return a
+
+warnCensor :: ([String] -> [String]) -> WarnParser a -> WarnParser a
+warnCensor f p = do
+  old <- getState
+  putState []
+  a <- p
+  new <- getState
+  putState (old ++ f new)
+  return a
+
+warnParse :: WarnParser a -> SourceName -> Text -> Either ParseError (a, [String])
+warnParse p src s = runParser p' [] src s
+  where
+    p' = liftA2 (,) p getState
+
+invertMap :: Ord b => Map a b -> Map b [a]
+invertMap = M.fromListWith (++) . over (each._2) (:[]) . map swap . M.toList
