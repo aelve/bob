900e8a41d22d8d2ee06133a5f8a5c6b9bc9b057c
Artyom <yom@artyom.me>
2015-09-14 19:57:20 +0300

    Add some comments and reorder things

--- tests/Main.hs
+++ tests/Main.hs
@@ -25,20 +25,47 @@ import Test.Tasty.HUnit
 import Bob
 
 
-with :: (a -> b -> c) -> Writer [(a, b)] () -> [c]
-with f = map (uncurry f) . execWriter
+main :: IO ()
+main = do
+  (rules, mbErrors) <- readRules
+  defaultMain $ testGroup "tests"
+    [ testCase "No warnings when loading rules" $
+        mbErrors @?= []
+    , arrowsTests rules
+    ]
 
+-- Here go some utility functions for tests.
+
+{- |
+This lets us use do notation to write lists of pairs. See
+<https://www.reddit.com/r/haskelltil/comments/3krrrr/enter_long_lists_with_do_notation_instead_of/> for a longer explanation.
+-}
 (.=) :: a -> b -> Writer [(a, b)] ()
 (.=) a b = tell [(a, b)]
 
+{- |
+Applies a function to all pairs in a list encoded with '.='.
+-}
+with :: (a -> b -> c) -> Writer [(a, b)] () -> [c]
+with f = map (uncurry f) . execWriter
+
+{- |
+Take a list of pairs (entity, patterns) and test that each pattern can find the entity. See the implementation of 'foundByIn' after the tests.
+-}
 testFoundBy :: [Rule] -> Writer [(Entity, [Pattern])] () -> TestTree
 testFoundBy rules = testGroup "finds" .
                     with (foundByIn rules)
 
+{- |
+Take a list of pairs (entity, patterns) and test that for each pattern that entity is the best match. See the implementation of 'bestMatchedByIn' after the tests.
+-}
 testBestMatchedBy :: [Rule] -> Writer [(Entity, [Pattern])] () -> TestTree
 testBestMatchedBy rules = testGroup "best matches" .
                           with (bestMatchedByIn rules)
 
+-- Here go tests themselves.
+
+-- | Tests for @arrows.rules@.
 arrowsTests :: [Rule] -> TestTree
 arrowsTests rules = testGroup "arrows"
   [ testGroup "ordinary arrows"
@@ -152,33 +179,63 @@ arrowsTests rules = testGroup "arrows"
     ]
   ]
 
-main :: IO ()
-main = do
-  (rules, mbErrors) <- readRules
-  defaultMain $ testGroup "tests"
-    [ testCase "No warnings when loading rules" $
-        mbErrors @?= []
-    , arrowsTests rules
-    ]
-
+{- |
+Test that a pattern finds an entity (e.g. “vv” finds “↡”, but doesn't find “ø”).
+-}
 findsIn :: [Rule] -> Pattern -> Entity -> TestTree
 findsIn rules pattern entity = testCase name $ found @? ""
   where
     name  = printf "‘%s’ finds ‘%s’" pattern entity
     found = entity `elem` map snd (matchAndSortRules pattern rules)
 
+{- |
+Test that a pattern doesn't find an entity (see 'findsIn').
+-}
 doesNotFindIn :: [Rule] -> Pattern -> Entity -> TestTree
 doesNotFindIn rules pattern entity = testCase name $ not found @? ""
   where
     name = printf "‘%s’ doesn't find ‘%s’" pattern entity
     found = entity `elem` map snd (matchAndSortRules pattern rules)
 
+{- |
+A version of 'findsIn' that does the test for many patterns at once (e.g. test that both “vv” and “↓↓” find “↡”).
+-}
 foundByIn :: [Rule] -> Entity -> [Pattern] -> TestTree
 foundByIn rules entity patterns = testGroup name cases
   where
     name = printf "finding ‘%s’" entity
     cases = [findsIn rules pattern entity | pattern <- patterns]
 
+{- |
+Test that an entity is a best match for a pattern (i.e. “vv” may find both “↡” and “⇊”, but it gives preference to “↡”).
+-}
+bestMatchIn :: [Rule] -> Pattern -> Entity -> TestTree
+bestMatchIn rules pattern entity = testCase name $ do
+  let matches = map snd (matchAndSortRules pattern rules)
+      surpassed = takeWhile (/= entity) matches
+      found = entity `elem` matches
+  found @? printf "it doesn't even find ‘%s’" entity
+  null surpassed @? if length surpassed == 1
+    then printf "%s comes before ‘%s’" (listTexts surpassed) entity
+    else printf "%s come before ‘%s’"  (listTexts surpassed) entity
+  where
+    name = printf "‘%s’ finds ‘%s’ as 1st result" pattern entity
+
+{- |
+A version of Test that an entity is a best match for all of the given patterns.
+-}
+bestMatchedByIn :: [Rule] -> Entity -> [Pattern] -> TestTree
+bestMatchedByIn rules entity patterns
+  | [pattern] <- patterns = bestMatchIn rules pattern entity
+  | otherwise             = testGroup name cases
+  where
+    name = printf "all of %s are best matches for ‘%s’"
+                  (listTexts patterns) entity
+    cases = [bestMatchIn rules pattern entity | pattern <- patterns]
+
+{- |
+Test that a pattern has some order of preference for entities it finds (it might find other entities as well, but given ones have to come in the order they are specified).
+-}
 partialOrderIn :: [Rule] -> Pattern -> [Entity] -> TestTree
 partialOrderIn rules pattern entities = testGroup name tests
   where
@@ -194,27 +251,10 @@ partialOrderIn rules pattern entities = testGroup name tests
         isJust mbIndex2 @? printf "it doesn't even find ‘%s’" e2
         mbIndex1 < mbIndex2 @? ""
 
-bestMatchIn :: [Rule] -> Pattern -> Entity -> TestTree
-bestMatchIn rules pattern entity = testCase name $ do
-  let matches = map snd (matchAndSortRules pattern rules)
-      surpassed = takeWhile (/= entity) matches
-      found = entity `elem` matches
-  found @? printf "it doesn't even find ‘%s’" entity
-  null surpassed @? if length surpassed == 1
-    then printf "%s comes before ‘%s’" (listTexts surpassed) entity
-    else printf "%s come before ‘%s’"  (listTexts surpassed) entity
-  where
-    name = printf "‘%s’ finds ‘%s’ as 1st result" pattern entity
-
-bestMatchedByIn :: [Rule] -> Entity -> [Pattern] -> TestTree
-bestMatchedByIn rules entity patterns
-  | [pattern] <- patterns = bestMatchIn rules pattern entity
-  | otherwise             = testGroup name cases
-  where
-    name = printf "all of %s are best matches for ‘%s’"
-                  (listTexts patterns) entity
-    cases = [bestMatchIn rules pattern entity | pattern <- patterns]
-
+{- |
+>>> listTexts ["a", "b", "c"]
+"‘a’ ‘b’ ‘c’"
+-}
 listTexts :: [Text] -> Text
 listTexts = T.unwords . map (\s -> "‘" <> s <> "’")
 
