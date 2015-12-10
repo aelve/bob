687c1b52f38daaf884026cf7430107ad8aeca3f3
Artyom <yom@artyom.me>
2015-12-10 17:05:22 +0300

    Switch from Tasty to Hspec

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,17 +1,17 @@
 {-# LANGUAGE
 OverloadedStrings,
-OverloadedLists
+OverloadedLists,
+NoImplicitPrelude
   #-}
 
 
 module Main where
 
 
+-- General
+import BasePrelude
 -- Testing
-import Test.Tasty
-import Test.Tasty.HUnit
--- Lists
-import Data.List (intercalate, dropWhileEnd)
+import Test.Hspec
 -- Text
 import Data.Text (Text)
 import qualified Data.Text as T
@@ -22,291 +22,286 @@ import Bob
 main :: IO ()
 main = do
   (_, _, mbErrors) <- readData
-  defaultMain $ testGroup "tests" [
-    parsingTests,
-    searchTests,
-    behaviorTests,
-    matcherTests,
-    generatorTests,
-    warningTests,
-    testCase "No warnings when loading rules" $
-      null mbErrors @? unlines mbErrors ]
+  hspec $ do
+    parsingTests
+    searchTests
+    behaviorTests
+    matcherTests
+    generatorTests
+    warningTests
+    specify "rules are loaded without warnings" $
+      unless (null mbErrors) $
+        expectationFailure (unlines mbErrors)
 
-parsingTests :: TestTree
-parsingTests = testGroup "parsing"
-  [ testCase "Simplest rule can be parsed" $ do
-      rules <- testReadRules "a = 1: b\n"
-      rules @?= [Rule Nothing [("b", [("a", Top 1)])]]
+parsingTests :: Spec
+parsingTests = context "parsing:" $ do
+  specify "parse the simplest rule" $ do
+    rules <- testReadRules "a = 1: b\n"
+    rules `shouldBe` [Rule Nothing [("b", [("a", Top 1)])]]
 
-  , testCase "2 rules" $ do
-      rules <- testReadRules $ T.unlines [
-        "a = 1: b",
-        "# whatever",
-        "",
-        "# whatever",
-        "x = X: y" ]
-      rules @?= [
-        Rule Nothing [("b", [("a", Top 1)])],
-        Rule Nothing [("y", [("x", Whatever)])]]
+  specify "parse 2 rules" $ do
+    rules <- testReadRules $ T.unlines [
+      "a = 1: b",
+      "# whatever",
+      "",
+      "# whatever",
+      "x = X: y" ]
+    rules `shouldBe` [
+      Rule Nothing [("b", [("a", Top 1)])],
+      Rule Nothing [("y", [("x", Whatever)])]]
 
-  , testCase "Newline at the end of file isn't needed" $ do
-      rules <- testReadRules "a = 1: b"
-      rules @?= [
-        Rule Nothing [("b", [("a", Top 1)])]]
+  specify "don't require newline at the end of file" $ do
+    rules <- testReadRules "a = 1: b"
+    rules `shouldBe` [
+      Rule Nothing [("b", [("a", Top 1)])]]
 
-  , testCase "A file can be empty" $ do
-      rules <- testReadRules ""
-      rules @?= []
+  specify "accept an empty file" $ do
+    rules <- testReadRules ""
+    rules `shouldBe` []
 
-  , testCase "A file can contain just a comment" $ do
-      rules <- testReadRules "# comment"
-      rules @?= []
+  specify "accept a file with just a comment" $ do
+    rules <- testReadRules "# comment"
+    rules `shouldBe` []
 
-  , testCase "Priority can't be 0" $ do
-      err <- testReadRulesButFail "a = 0: b"
-      err @?= unlines [
-        "line 1, column 6:",
-        "expecting rest of integer",
-        "priority can't be 0" ]
-  ]
+  specify "priority can't be 0" $ do
+    err <- testReadRulesButFail "a = 0: b"
+    err `shouldBe` unlines [
+      "line 1, column 6:",
+      "expecting rest of integer",
+      "priority can't be 0" ]
 
-searchTests :: TestTree
-searchTests = testGroup "search"
-  [ testCase "Entities with equal priority are ordered alphabetically" $ do
-      rules <- testReadRules $ T.unlines [
-        "e = 5: x",
-        "b = 5: x",
-        "c = 5: x",
-        "",
-        "#### hi",
-        "d = 5: x",
-        "a = 5: x" ]
-      matchRules "x" rules @?= [
-        ((Just "hi", "a"), Top 5),
-        ((Nothing,   "b"), Top 5),
-        ((Nothing,   "c"), Top 5),
-        ((Just "hi", "d"), Top 5),
-        ((Nothing,   "e"), Top 5)]
-  ]
+searchTests :: Spec
+searchTests = context "search:" $ do
+  specify "order entities with equal priority alphabetically" $ do
+    rules <- testReadRules $ T.unlines [
+      "e = 5: x",
+      "b = 5: x",
+      "c = 5: x",
+      "",
+      "#### hi",
+      "d = 5: x",
+      "a = 5: x" ]
+    matchRules "x" rules `shouldBe` [
+      ((Just "hi", "a"), Top 5),
+      ((Nothing,   "b"), Top 5),
+      ((Nothing,   "c"), Top 5),
+      ((Just "hi", "d"), Top 5),
+      ((Nothing,   "e"), Top 5)]
 
-behaviorTests :: TestTree
-behaviorTests = testGroup "behavior"
-  [ testCase "####-notes should work" $ do
-      rules <- testReadRules $ T.unlines [
-        "####  blah",
-        "a = 1: b" ]
-      rules @?= [
-        Rule (Just "blah") [("b", [("a", Top 1)])]]
+behaviorTests :: Spec
+behaviorTests = context "behavior:" $ do
+  specify "####-notes work" $ do
+    rules <- testReadRules $ T.unlines [
+      "####  blah",
+      "a = 1: b" ]
+    rules `shouldBe` [
+      Rule (Just "blah") [("b", [("a", Top 1)])]]
 
-  , testCase "Later priorities should count, not earlier" $ do
-      rules <- testReadRules $ T.unlines [
-        "a = 1: one two",
-        "    2: two" ]
-      rules @?= [
-        Rule Nothing [
-          ("one", [("a", Top 1)]),
-          ("two", [("a", Top 2)])]]
+  specify "later priorities override earlier ones" $ do
+    rules <- testReadRules $ T.unlines [
+      "a = 1: one two",
+      "    2: two" ]
+    rules `shouldBe` [
+      Rule Nothing [
+        ("one", [("a", Top 1)]),
+        ("two", [("a", Top 2)])]]
 
-  , testCase "A matcher can generate several entities for a pattern" $ do
-      rules <- testReadRules $ T.unlines [
-        "zip Aa",
-        "    Xx",
-        "    2: {(A a) ()}" ]
-      rules @?= [
-        Rule Nothing [
-          ("AA",[("X",Top 2)]),
-          ("Aa",[("X",Top 2),("x",Top 2)]),
-          ("aA",[("X",Top 2),("x",Top 2)]),
-          ("aa",[("x",Top 2)])]]
-  ]
+  specify "a matcher can generate several entities for a pattern" $ do
+    rules <- testReadRules $ T.unlines [
+      "zip Aa",
+      "    Xx",
+      "    2: {(A a) ()}" ]
+    rules `shouldBe` [
+      Rule Nothing [
+        ("AA",[("X",Top 2)]),
+        ("Aa",[("X",Top 2),("x",Top 2)]),
+        ("aA",[("X",Top 2),("x",Top 2)]),
+        ("aa",[("x",Top 2)])]]
 
-matcherTests :: TestTree
-matcherTests = testGroup "matchers"
-  [ testCase "zip" $ do
-      rules <- testReadRules $ T.unlines [
-        "zip ab",
-        "    AB",
-        "    1: ()+",
-        "    2: +()" ]
-      rules @?= [
-        Rule Nothing [
-          ("a+",[("A",Top 1)]),
-          ("b+",[("B",Top 1)]),
-          ("+a",[("A",Top 2)]),
-          ("+b",[("B",Top 2)])]]
+matcherTests :: Spec
+matcherTests = context "matchers:" $ do
+  specify "zip" $ do
+    rules <- testReadRules $ T.unlines [
+      "zip ab",
+      "    AB",
+      "    1: ()+",
+      "    2: +()" ]
+    rules `shouldBe` [
+      Rule Nothing [
+        ("a+",[("A",Top 1)]),
+        ("b+",[("B",Top 1)]),
+        ("+a",[("A",Top 2)]),
+        ("+b",[("B",Top 2)])]]
 
-  , testCase "many-to-one" $ do
-      rules <- testReadRules $ T.unlines [
-        "x = 1: a",
-        "    2: b" ]
-      rules @?= [
-        Rule Nothing [
-          ("a",[("x",Top 1)]),
-          ("b",[("x",Top 2)])]]
+  specify "many-to-one" $ do
+    rules <- testReadRules $ T.unlines [
+      "x = 1: a",
+      "    2: b" ]
+    rules `shouldBe` [
+      Rule Nothing [
+        ("a",[("x",Top 1)]),
+        ("b",[("x",Top 2)])]]
 
-  , testCase "order" $ do
-      rules <- testReadRules $ T.unlines [
-        "(x y) : a b" ]
-      rules @?= [
-        Rule Nothing [
-          ("x", [("a",Top 1), ("b",Top 2)]),
-          ("y", [("a",Top 1), ("b",Top 2)])]]
-  ]
+  specify "order" $ do
+    rules <- testReadRules $ T.unlines [
+      "(x y) : a b" ]
+    rules `shouldBe` [
+      Rule Nothing [
+        ("x", [("a",Top 1), ("b",Top 2)]),
+        ("y", [("a",Top 1), ("b",Top 2)])]]
 
-generatorTests :: TestTree
-generatorTests = testGroup "generators"
-  [ testCase "row of generators" $ do
-      rules <- testReadRules $ T.unlines [
-        "x = 1: a (b c)" ]
-      rules @?= [
-        Rule Nothing [
-          ("a",[("x",Top 1)]),
-          ("b",[("x",Top 1)]),
-          ("c",[("x",Top 1)])]]
+generatorTests :: Spec
+generatorTests = context "generators:" $ do
+  specify "row of generators" $ do
+    rules <- testReadRules $ T.unlines [
+      "x = 1: a (b c)" ]
+    rules `shouldBe` [
+      Rule Nothing [
+        ("a",[("x",Top 1)]),
+        ("b",[("x",Top 1)]),
+        ("c",[("x",Top 1)])]]
 
-  , testCase "sequence" $ do
-      rules <- testReadRules $ T.unlines [
-        "x = 1: +(a b){c d}-" ]
-      rules @?= [
-        Rule Nothing [
-          ("+acd-",[("x",Top 1)]),
-          ("+adc-",[("x",Top 1)]),
-          ("+bcd-",[("x",Top 1)]),
-          ("+bdc-",[("x",Top 1)])]]
+  specify "sequence" $ do
+    rules <- testReadRules $ T.unlines [
+      "x = 1: +(a b){c d}-" ]
+    rules `shouldBe` [
+      Rule Nothing [
+        ("+acd-",[("x",Top 1)]),
+        ("+adc-",[("x",Top 1)]),
+        ("+bcd-",[("x",Top 1)]),
+        ("+bdc-",[("x",Top 1)])]]
 
-  , testCase "literal" $ do
-      rules <- testReadRules $ T.unlines [
-        "x = 1: a 'bcd' '''' '()'" ]
-      rules @?= [
-        Rule Nothing [
-          ("a"  ,[("x",Top 1)]),
-          ("bcd",[("x",Top 1)]),
-          ("'"  ,[("x",Top 1)]),
-          ("()" ,[("x",Top 1)])]]
+  specify "literal" $ do
+    rules <- testReadRules $ T.unlines [
+      "x = 1: a 'bcd' '''' '()'" ]
+    rules `shouldBe` [
+      Rule Nothing [
+        ("a"  ,[("x",Top 1)]),
+        ("bcd",[("x",Top 1)]),
+        ("'"  ,[("x",Top 1)]),
+        ("()" ,[("x",Top 1)])]]
 
-  , testCase "single generator" $ do
-      rules <- testReadRules $ T.unlines [
-        "x = 1: (x y)" ]
-      rules @?= [
-        Rule Nothing [
-          ("x",[("x",Top 1)]),
-          ("y",[("x",Top 1)])]]
+  specify "single generator" $ do
+    rules <- testReadRules $ T.unlines [
+      "x = 1: (x y)" ]
+    rules `shouldBe` [
+      Rule Nothing [
+        ("x",[("x",Top 1)]),
+        ("y",[("x",Top 1)])]]
 
-  , testCase "variable" $ do
-      rules <- testReadRules $ T.unlines [
-        "zip a",
-        "    A",
-        "    1: ()" ]
-      rules @?= [
-        Rule Nothing [("a",[("A",Top 1)])]]
+  specify "variable" $ do
+    rules <- testReadRules $ T.unlines [
+      "zip a",
+      "    A",
+      "    1: ()" ]
+    rules `shouldBe` [
+      Rule Nothing [("a",[("A",Top 1)])]]
 
-  , testCase "any-of" $ do
-      rules <- testReadRules $ T.unlines [
-        "x = 1: (a b)" ]
-      rules @?= [
-        Rule Nothing [
-          ("a",[("x",Top 1)]),
-          ("b",[("x",Top 1)])]]
+  specify "any-of" $ do
+    rules <- testReadRules $ T.unlines [
+      "x = 1: (a b)" ]
+    rules `shouldBe` [
+      Rule Nothing [
+        ("a",[("x",Top 1)]),
+        ("b",[("x",Top 1)])]]
 
-  , testCase "permutation" $ do
-      rules <- testReadRules $ T.unlines [
-        "x = 1: {a b}" ]
-      rules @?= [
-        Rule Nothing [
-          ("ab",[("x",Top 1)]),
-          ("ba",[("x",Top 1)])]]
+  specify "permutation" $ do
+    rules <- testReadRules $ T.unlines [
+      "x = 1: {a b}" ]
+    rules `shouldBe` [
+      Rule Nothing [
+        ("ab",[("x",Top 1)]),
+        ("ba",[("x",Top 1)])]]
 
-  , testCase "reference" $ do
-      rules <- testReadRules $ T.unlines [
-        "a = 1: a b",
-        "",
-        "x = 1: `a``'a'`" ]
-      rules @?= [
-        Rule Nothing [
-          ("a",[("a",Top 1)]),
-          ("b",[("a",Top 1)])],
-        Rule Nothing [
-          ("aa",[("x",Top 1)]),
-          ("ab",[("x",Top 1)]),
-          ("ba",[("x",Top 1)]),
-          ("bb",[("x",Top 1)])]]
-  ]
+  specify "reference" $ do
+    rules <- testReadRules $ T.unlines [
+      "a = 1: a b",
+      "",
+      "x = 1: `a``'a'`" ]
+    rules `shouldBe` [
+      Rule Nothing [
+        ("a",[("a",Top 1)]),
+        ("b",[("a",Top 1)])],
+      Rule Nothing [
+        ("aa",[("x",Top 1)]),
+        ("ab",[("x",Top 1)]),
+        ("ba",[("x",Top 1)]),
+        ("bb",[("x",Top 1)])]]
 
-warningTests :: TestTree
-warningTests = testGroup "warnings"
-  [ testCase "Warn when arguments of 'zip' have unequal lengths" $ do
-      (_, warnings) <- testReadRulesAndWarnings $ T.unlines [
-        "zip abc",
-        "    wxyz",
-        "    1: ()" ]
-      warnings @?= unlines [
-        "warnings in rule at line 1, column 1:",
-        "  lengths of zipped rows don't match" ]
+warningTests :: Spec
+warningTests = context "warnings:" $ do
+  specify "arguments of 'zip' have unequal lengths" $ do
+    (_, warnings) <- testReadRulesAndWarnings $ T.unlines [
+      "zip abc",
+      "    wxyz",
+      "    1: ()" ]
+    warnings `shouldBe` unlines [
+      "warnings in rule at line 1, column 1:",
+      "  lengths of zipped rows don't match" ]
 
-  , testCase "Warn when undefined things are referenced" $ do
-      (_, warnings) <- testReadRulesAndWarnings $ T.unlines [
-        "a   : 1 2 3",
-        "`e` : x y z" ]
-      warnings @?= unlines [
-        "warnings in rule at line 1, column 1:",
-        "  ‘e’ was referenced but wasn't defined yet" ]
+  specify "an undefined thing is referenced" $ do
+    (_, warnings) <- testReadRulesAndWarnings $ T.unlines [
+      "a   : 1 2 3",
+      "`e` : x y z" ]
+    warnings `shouldBe` unlines [
+      "warnings in rule at line 1, column 1:",
+      "  ‘e’ was referenced but wasn't defined yet" ]
 
-  , testCase "Warn when no value can be provided for a variable" $ do
-      (_, warnings) <- testReadRulesAndWarnings $ T.unlines [
-        "a  : 1 2 3",
-        "() : x y z" ]
-      warnings @?= unlines [
-        "warnings in rule at line 1, column 1:",
-        "  there's a variable in the rule but no value provided for it" ]
+  specify "no value can be provided for a variable" $ do
+    (_, warnings) <- testReadRulesAndWarnings $ T.unlines [
+      "a  : 1 2 3",
+      "() : x y z" ]
+    warnings `shouldBe` unlines [
+      "warnings in rule at line 1, column 1:",
+      "  there's a variable in the rule but no value provided for it" ]
 
-  , testCase "Warn when empty patterns are encountered" $ do
-      (_, warnings) <- testReadRulesAndWarnings $ T.unlines [
-        "a  : 1 2 3",
-        "'' : x y z" ]
-      warnings @?= unlines [
-        "warnings in rule at line 1, column 1:",
-        "  matcher #2 contains an empty pattern" ]
+  specify "an empty pattern is encountered" $ do
+    (_, warnings) <- testReadRulesAndWarnings $ T.unlines [
+      "a  : 1 2 3",
+      "'' : x y z" ]
+    warnings `shouldBe` unlines [
+      "warnings in rule at line 1, column 1:",
+      "  matcher #2 contains an empty pattern" ]
 
-  , testCase "Warn when priorities aren't satisfied" $ do
-      (_, warnings) <- testReadRulesAndWarnings $ T.unlines [
-        "e1 = 1: x",
-        "     2: y",
-        "e2 = 1: x",
-        "     2: y",
-        "e3 = 2: x",
-        "     1: y" ]
-      warnings @?= unlines [
-        "‘x’ finds:",
-        "  2 entities with priority 1 or less: e1 e2",
-        "  3 entities with priority 2 or less: e1 e2 e3",
-        "",
-        "‘y’ finds:",
-        "  3 entities with priority 2 or less: e3 e1 e2" ]
+  specify "priorities aren't satisfied" $ do
+    (_, warnings) <- testReadRulesAndWarnings $ T.unlines [
+      "e1 = 1: x",
+      "     2: y",
+      "e2 = 1: x",
+      "     2: y",
+      "e3 = 2: x",
+      "     1: y" ]
+    warnings `shouldBe` unlines [
+      "‘x’ finds:",
+      "  2 entities with priority 1 or less: e1 e2",
+      "  3 entities with priority 2 or less: e1 e2 e3",
+      "",
+      "‘y’ finds:",
+      "  3 entities with priority 2 or less: e3 e1 e2" ]
 
-  , testCase "Warn when an entity can't be found using only ASCII" $ do
-      (_, warnings) <- testReadRulesAndWarnings $ T.unlines [
-        "bad1 = 1: x€y",     -- bad (non-ASCII)
-        "bad2 = 1: ä",       -- bad (non-ASCII too)
-        "okay1 = 1: ₮ ***",  -- okay (there's a good pattern available)
-        "okay2 = 1: ₹",      -- okay (there's a good pattern in another rule)
-        "",
-        "okay2 = 1: rupee" ]
-      warnings @?= unlines [
-        "‘bad1’ can't be found with ASCII; found by: x€y",
-        "",
-        "‘bad2’ can't be found with ASCII; found by: ä" ]
-  ]
+  specify "an entity can't be found using only ASCII" $ do
+    (_, warnings) <- testReadRulesAndWarnings $ T.unlines [
+      "bad1 = 1: x€y",     -- bad (non-ASCII)
+      "bad2 = 1: ä",       -- bad (non-ASCII too)
+      "okay1 = 1: ₮ ***",  -- okay (there's a good pattern available)
+      "okay2 = 1: ₹",      -- okay (there's a good pattern in another rule)
+      "",
+      "okay2 = 1: rupee" ]
+    warnings `shouldBe` unlines [
+      "‘bad1’ can't be found with ASCII; found by: x€y",
+      "",
+      "‘bad2’ can't be found with ASCII; found by: ä" ]
 
 testReadRules :: Text -> IO [Rule]
 testReadRules rulesString =
   case readRuleFile rulesString of
     Left err -> do
-      assertFailure (show err)
+      expectationFailure (show err)
       error "test failed"
     Right (rules, []) ->
       return rules
     Right (_rules, warnings) -> do
-      assertFailure (unparagraphs warnings)
+      expectationFailure (unparagraphs warnings)
       error "test failed"
 
 testReadRulesButFail :: Text -> IO String
@@ -318,14 +313,14 @@ testReadRulesButFail rulesString =
       -- always adds a newline, so let's add a newline too.
       return (show err ++ "\n")
     Right _ -> do
-      assertFailure "there was no error"
+      expectationFailure "there was no error"
       error "test failed"
 
 testReadRulesAndWarnings :: Text -> IO ([Rule], String)
 testReadRulesAndWarnings rulesString =
   case readRuleFile rulesString of
     Left err -> do
-      assertFailure (show err)
+      expectationFailure (show err)
       error "test failed"
     Right (rules, warnings) ->
       return (rules, unparagraphs warnings)
